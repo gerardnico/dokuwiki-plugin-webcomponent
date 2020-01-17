972bd77b455a12ece7ab80c7f7d21513a802a848
gerardnico <gerardnico@gmail.com>
2020-01-17 10:46:58 +0100

    task added

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,16 +1,26 @@
-port = 8082;
 
-module.exports = {
-    // server: {
-    //     command: `cross-env NODE_ENV=development webpack-dev-server --progress -d --inline --open --port ${port}`,
-    //     port: port,
-    //     launchTimeout: 30000
-    // },
+jest_puppeteer_conf = {
     launch: {
-        // headless: false, // for debug:  to see what the browser is displaying
-        // slowMo: 250, // slow down by 250ms
-        devtools: true, // This lets you debug code in the application code browser
         timeout: 30000,
         dumpio: true // Whether to pipe the browser process stdout and stderr 
     }
 }
+
+var debug = (typeof v8debug === 'object');
+if (debug) {
+    jest_puppeteer_conf.launch.headless = false; // for debug:  to see what the browser is displaying
+    jest_puppeteer_conf.launch.slowMo = 250;  // slow down by 250ms for each step
+    jest_puppeteer_conf.launch.devtools = true; // This lets you debug code in the application code browser
+}
+
+
+if (require('ci-info').isCI) {
+    jest_puppeteer_conf.server = {
+        command: `webpack-dev-server --progress -d --inline --open --port ${port}`,
+        port: port,
+        launchTimeout: 30000
+
+    };
+}
+
+module.exports = jest_puppeteer_conf;
