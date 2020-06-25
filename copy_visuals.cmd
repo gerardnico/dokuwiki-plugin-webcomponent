bd37771c339cfc6b5399bca88cd92dcde137b705
gerardnico <gerardnico@gmail.com>
2020-06-25 16:46:32 +0200

    New doc structure

--- copy_visuals.cmd
+++ copy_visuals.cmd
@@ -7,25 +7,25 @@ echo Backup of the dokuwiki pages that serves as visual into the directory %WEB_
 echo .
 echo .
 SET DOKU_ROOT=%SCRIPT_PATH%..\..\..
-SET DOKU_DATA=%DOKU_ROOT%\dokudata
+SET DOKU_DATA=%DOKU_ROOT%\dataweb
 
-SET WEB_COMPO_VISUAL_PAGES=%DOKU_DATA%\pages\dokuwiki\webcomponent
+SET WEB_COMPO_VISUAL_PAGES=%DOKU_DATA%\pages
 SET WEB_COMPO_VISUAL_PAGES_DST=%WEB_COMPO_VISUAL_BACKUP%\pages
 
 echo Copying the pages:
 echo   * from %WEB_COMPO_VISUAL_PAGES%
 echo   * to %WEB_COMPO_VISUAL_PAGES_DST%
 echo .
-call copy %WEB_COMPO_VISUAL_PAGES% %WEB_COMPO_VISUAL_PAGES_DST%
+call xcopy %WEB_COMPO_VISUAL_PAGES% %WEB_COMPO_VISUAL_PAGES_DST% /s /e /h /y
 echo .
 
-SET WEB_COMPO_VISUAL_MEDIAS=%DOKU_DATA%\media\dokuwiki\webcomponent
+SET WEB_COMPO_VISUAL_MEDIAS=%DOKU_DATA%\media
 SET WEB_COMPO_VISUAL_MEDIAS_DST=%WEB_COMPO_VISUAL_BACKUP%\media
 
 echo Copying the images:
 echo   * from %WEB_COMPO_VISUAL_MEDIAS%
 echo   * to %WEB_COMPO_VISUAL_MEDIAS_DST%
 echo .
-call copy /V /Y %WEB_COMPO_VISUAL_MEDIAS% %WEB_COMPO_VISUAL_MEDIAS_DST%
+call xcopy %WEB_COMPO_VISUAL_MEDIAS% %WEB_COMPO_VISUAL_MEDIAS_DST% /s /e /h /y
 echo .
 echo Done
